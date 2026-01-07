dee53b3ef713967642c0ffbdeb0dc9e228a3ca47
Goerge de Beras <georgeberas@hotmail.com>
2026-01-06 22:21:43 -0400

    Add header and message for new version loaded

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<h1 style="background: red; color: white; font-size: 50px; z-index: 9999; position: fixed;">VERSION NUEVA CARGADA</h1>
+
+// Si es PHP
+die("VERSION NUEVA CARGADA - EL CÓDIGO SE EJECUTA");
+
+
+
 <?php
 // Root index.php
 require_once __DIR__ . '/public/index.php';
