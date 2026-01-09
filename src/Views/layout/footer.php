17ba902e2109a6996f0e56248d61b56f04025aa4
Georgedeberas <georgeberas@hotmail.com>
2026-01-08 21:32:43 -0400

    UI Fix: Restoring Mobile Bar (Orange Transparent, 3 Icons, Green WhatsApp)

--- src/Views/layout/footer.php
+++ src/Views/layout/footer.php
@@ -151,3 +151,46 @@ if (!isset($conf)) {
             </div>
         </div>
     </div>
+</footer>
+
+<!-- Mobile Bottom App Bar (Sticky & Modern) -->
+<div
+    class="fixed bottom-0 left-0 w-full bg-[#ff8c00]/80 backdrop-blur-md border-t border-white/20 z-[9999] md:hidden pb-safe rounded-t-2xl shadow-[0_-4px_20px_rgba(0,0,0,0.15)]">
+    <div class="flex justify-between items-center px-12 h-[45px]">
+
+        <!-- Inicio -->
+        <a href="/" class="flex items-center justify-center text-white hover:text-white/80 transition-opacity w-10"
+            aria-label="Inicio">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                <path
+                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
+            </svg>
+        </a>
+
+        <!-- Tours (Grid Icon - 4 Squares) -->
+        <a href="/#tours"
+            class="flex items-center justify-center text-white hover:text-white/80 transition-opacity w-10"
+            aria-label="Tours">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                <path
+                    d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+            </svg>
+        </a>
+
+        <!-- WhatsApp (Green Circle Button with White Icon) -->
+        <a href="<?= $waLink ?>" target="_blank"
+            class="flex items-center justify-center w-9 h-9 rounded-full bg-[#25D366] hover:scale-110 transition-transform shadow-md"
+            aria-label="WhatsApp">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 16 16">
+                <path
+                    d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"
+                    transform="scale(0.75) translate(2,2)" />
+            </svg>
+        </a>
+
+    </div>
+</div>
+
+</body>
+
+</html>
