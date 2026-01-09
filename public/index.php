24386a6640502aae98ad332834a42df618233999
Georgedeberas <georgeberas@hotmail.com>
2026-01-09 01:24:31 -0400

    chore: disable debug mode for production integrity

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 
 // Production Error Handling
 // Production Error Handling
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../error_log.php'); // Log local protegido
